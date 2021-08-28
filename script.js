50585f4466fb701fdd8368f14722a999360c89d8
norihitoishida <norihitoishida@gmail.com>
2021-08-28 16:25:08 +0900

    update

--- script.js
+++ script.js
@@ -25,39 +25,42 @@ function handleFiles(files) {
     let gallery = document.getElementById("imgGallery");
     for (let i = 0; i < files.length; i++) {
         
-        
+        // コンテナ全体を作る
         let container = document.createElement("div");
         container.id = "imgContainer";
         gallery.appendChild(container);
         
+        // プレビュー欄を作る
         let preview = document.createElement("div");
         preview.id = "imgPreview";
-        preview.innerText = "プレビュー";
         container.appendChild(preview);
 
+        // imageを読み込み
+        let img = document.createElement("img");
+        let file = files[i];
+        img.file = file;
+        
+        // リサイズ
+        // let ratio = img.width / img.height;
+        img.height = 140;
+
+        // 貼り付け
+        preview.appendChild(img);
+        let reader = new FileReader();
+        reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+        reader.readAsDataURL(file);
+        
+        // 設定ボックス(回転・削除)
         let settingbox = document.createElement("div");
         settingbox.id = "settingBox";
         settingbox.innerText = "設定";
         container.appendChild(settingbox);
 
+        // コメント欄
         let commentbox = document.createElement("textarea");
         commentbox.id = "commentBox";
         commentbox.placeholder = "コメント";
         container.appendChild(commentbox);
-        
-        /*
-        let file = files[i];
-        let img = document.createElement("img");
-        // let preview = document.createElement("imgContainer");
-
-        img.classList.add("obj");
-        img.file = file;
-        preview.appendChild(img);
-        
-        const reader = new FileReader();
-        reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-        reader.readAsDataURL(file);
-        */
     }
 }
 
