6112bd64f6ea3a1d6f956a52425266f690a45dd8
norihitoishida <norihitoishida@gmail.com>
2021-08-28 21:06:18 +0900

    クリアボタンの中身を実装

--- script.js
+++ script.js
@@ -24,6 +24,26 @@ function del() {
     delContainer.remove();
 }
 
+function rotateLeft() {
+    uniqueid = this.id.slice(17,);
+    let targetimg = document.getElementById(`img_${uniqueid}`);
+    let angle = Number(targetimg.name);
+    angle = (angle - 90) % 360;
+    targetimg.name = angle;
+    // targetimg.width = 140;
+    targetimg.style.transform = `rotate(${angle}deg)`;
+}
+
+function rotateRight() {
+    uniqueid = this.id.slice(18,);
+    let targetimg = document.getElementById(`img_${uniqueid}`);
+    let angle = Number(targetimg.name);
+    angle = (angle + 90) % 360;
+    targetimg.name = angle;
+    // targetimg.width = 140;
+    targetimg.style.transform = `rotate(${angle}deg)`;
+}
+
 function handleFiles(files) {
     n_drop++;
     let gallery = document.getElementById("imgGallery");
@@ -45,6 +65,8 @@ function handleFiles(files) {
 
         // imageを読み込み
         let img = document.createElement("img");
+        img.id = `img_${uniqueid}`;
+        img.name = 0; //角度
         let file = files[i];
         img.file = file;
         
@@ -68,20 +90,20 @@ function handleFiles(files) {
         rotatebutton_left.id = `rotateButtonLeft_${uniqueid}`;
         rotatebutton_left.textContent = "左回転";
         settingbox.appendChild(rotatebutton_left);
+        rotatebutton_left.addEventListener("click", rotateLeft);
 
         let rotatebutton_right = document.createElement("button");
         rotatebutton_right.id = `rotateButtonRight_${uniqueid}`;
         rotatebutton_right.textContent = "右回転";
         settingbox.appendChild(rotatebutton_right);
+        rotatebutton_right.addEventListener("click", rotateRight);
 
         // 設定ボックスに削除ボタンを追加
         let delbutton = document.createElement("button");
         delbutton.id = `deleteButton_${uniqueid}`;
         delbutton.textContent = "クリア";
         settingbox.appendChild(delbutton);
-
         delbutton.addEventListener("click", del);
-
 
         // コメント欄
         let commentbox = document.createElement("textarea");
